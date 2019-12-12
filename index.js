61735f85724d1b99e690e0bc95cb4844a825d30c
Michael Vogel <voguesie@alumni.stanford.edu>
2019-12-12 02:18:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
+function addToCart(item) { // Adds an object representing an item added to the card.
+                           // The object contains two key:value pairs, one for the item's name, the other for it's price.
  // write your code here
+ var price = Math.floor(Math.random()*100+1);
+ var newItem = {itemName: item, itemPrice: price};
 }
 
 function viewCart() {
