c689fa2a28d165dfa267659bdaf96e01a65bea97
Michael Vogel <voguesie@alumni.stanford.edu>
2019-12-12 04:19:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,7 +53,6 @@ function placeOrder(cardNumber) { // Accepts cardNumber, gives total price, and 
   if (isNaN(cardNumber)) { // Checks if a number was entered, and returns default statement if not.
   return "Sorry, we don't have a credit card on file for you."
 } else {
-  var totalCost = total();
-  return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`, cart.splice(0, cart.length)
+  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`, cart.splice(0, cart.length)
 }
 }
